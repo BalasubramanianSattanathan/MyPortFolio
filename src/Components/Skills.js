f2c24c908b567ce43271f0839f7a009c4b222afb
Bala Subramanian <bala-subramanian.s@capgemini.com>
2024-07-13 16:03:34 +0530

    Unwanted code removed

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { CgCPlusPlus } from "react-icons/cg";
 import { FaReact, FaGitAlt, FaGithub, FaNpm, FaFigma, FaBootstrap } from "react-icons/fa";
 import { DiNodejs, DiJavascript1, DiHtml5, DiCss3 } from "react-icons/di";
-import { SiExpress, SiMongodb, SiPostman, SiVercel, SiRedux, SiMobx } from "react-icons/si";
+import { SiExpress, SiMongodb, SiPostman, SiRedux, SiMobx } from "react-icons/si";
 
 const Skills = ({ skill }) => {
   const icon = {
